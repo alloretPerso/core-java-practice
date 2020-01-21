ca8988bcfb21e996744d18fc7bdceb93136ac3ad
alexandre.lloret <alloret.perso@gmail.com>
2020-01-21 17:15:38 +0000

    Create a Node

---
+++ practiceofcore/src/main/java/practiceofcore/hashmap/Entry.java
@@ -0,0 +1,42 @@
+package practiceofcore.hashmap;
+
+public class Entry<K, V> {
+    final K key;
+    V value;
+    Entry<K, V> next;
+
+    public Entry(K key, V value, Entry<K, V> next) {
+        this.key = key;
+        this.value = value;
+        this.next = next;
+    }
+
+    @Override
+    public String toString() {
+        return "Entry{" +
+                "key=" + key +
+                ", value=" + value +
+                ", next=" + next +
+                '}';
+    }
+
+    public K getKey() {
+        return key;
+    }
+
+    public V getValue() {
+        return value;
+    }
+
+    public void setValue(V value) {
+        this.value = value;
+    }
+
+    public Entry<K, V> getNext() {
+        return next;
+    }
+
+    public void setNext(Entry<K, V> next) {
+        this.next = next;
+    }
+}
