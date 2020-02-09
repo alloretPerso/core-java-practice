d4e194ad1a70bce36de2be2613928ee78a37ff42
alexandre.lloret <alloret.perso@gmail.com>
2020-02-09 15:57:39 +0000

    Expanding the logic

--- Oop/src/com/company/neverNull/Article.java
+++ Oop/src/com/company/neverNull/Article.java
@@ -3,13 +3,21 @@ package com.company.neverNull;
 public class Article {
     private Warranty moneyBackGuarantee;
     private Warranty expressWarranty;
+    private Warranty effectiveExpressWarranty = Warranty.VOID;
 
-    public Article(Warranty moneyBackGuarantee, Warranty expressWarranty) {
+
+    private Article(Warranty moneyBackGuarantee, Warranty expressWarranty, Warranty effectiveExpressWarranty) {
         //Constructor preconditions
         if (moneyBackGuarantee == null) throw new IllegalArgumentException();
         if (expressWarranty == null) throw new IllegalArgumentException();
         this.moneyBackGuarantee = moneyBackGuarantee;
         this.expressWarranty = expressWarranty;
+        this.effectiveExpressWarranty = effectiveExpressWarranty;
+
+    }
+
+    public Article(Warranty moneyBackGuarantee, Warranty expressWarranty) {
+        this(moneyBackGuarantee, expressWarranty, Warranty.VOID);
     }
 
     public Warranty getMoneyBackGuarantee() {
@@ -17,6 +25,14 @@ public class Article {
     }
 
     public Warranty getExpressWarranty() {
-        return expressWarranty;
+        return effectiveExpressWarranty;
+    }
+
+    public Article withVisibleDamage() {
+        return new Article(Warranty.VOID, this.expressWarranty, this.effectiveExpressWarranty);
+    }
+
+    public Article notOperational() {
+        return new Article(this.moneyBackGuarantee, this.expressWarranty, this.expressWarranty);
     }
 }
--- Oop/src/com/company/neverNull/Demo.java
+++ Oop/src/com/company/neverNull/Demo.java
@@ -9,14 +9,18 @@ public class Demo {
     public void run() {
         // write your code here
         LocalDate sellingDate = LocalDate.now().minus(40, ChronoUnit.DAYS);
-        Warranty moneyBack1 = new TimeLimitedWarranty(sellingDate, Duration.ofDays(30));
+        Warranty moneyBack1 = new TimeLimitedWarranty(sellingDate, Duration.ofDays(60));
         Warranty warranty1 = new TimeLimitedWarranty(sellingDate, Duration.ofDays(365));
         Article item1 = new Article(moneyBack1, warranty1);
 
         this.claimWarranty(item1);
+        this.claimWarranty(item1.withVisibleDamage());
+        this.claimWarranty(item1.notOperational().withVisibleDamage());
+        this.claimWarranty(item1.notOperational());
 
         Article item2 = new Article(Warranty.VOID, Warranty.lifeTime(sellingDate));
         this.claimWarranty(item2);
+        this.claimWarranty(item2.withVisibleDamage().notOperational());
     }
 
     private void offerMoneyBack() {
