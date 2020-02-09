bd6e5c9288be8b07f98c04d58ded0d00b9338be0
alexandre.lloret <alloret.perso@gmail.com>
2020-02-09 13:38:52 +0000

    Step the demo

--- Oop/src/com/company/Demo.java
+++ Oop/src/com/company/Demo.java
@@ -1,10 +1,18 @@
 package com.company;
 
 public class Demo {
-    private void buy(Money wallet, Money Cost){
+    private void reserve(Money cost) {
+        System.out.println("Reserving an item costing" + cost);
+    }
+
+    private void buy(Money wallet, Money cost) {
+        boolean enoughMoney = wallet.compareTo(cost) >= 0;
+        this.reserve(cost);
+
 
     }
-    public void run(){
+
+    public void run() {
 
     }
 }
